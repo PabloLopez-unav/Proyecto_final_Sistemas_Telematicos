6fc5429a2a7b9302d712981649c7ac9019701799
El Consta <you@example.com>
2025-04-28 15:49:55 +0200

    Primer commit del proyecto con todos los proyectos MFC

---
+++ Cliente/ClienteDlg.h
@@ -0,0 +1,33 @@
+
+// ClienteDlg.h : header file
+//
+
+#pragma once
+
+
+// CClienteDlg dialog
+class CClienteDlg : public CDialogEx
+{
+// Construction
+public:
+	CClienteDlg(CWnd* pParent = nullptr);	// standard constructor
+
+// Dialog Data
+#ifdef AFX_DESIGN_TIME
+	enum { IDD = IDD_CLIENTE_DIALOG };
+#endif
+
+	protected:
+	virtual void DoDataExchange(CDataExchange* pDX);	// DDX/DDV support
+
+
+// Implementation
+protected:
+	HICON m_hIcon;
+
+	// Generated message map functions
+	virtual BOOL OnInitDialog();
+	afx_msg void OnPaint();
+	afx_msg HCURSOR OnQueryDragIcon();
+	DECLARE_MESSAGE_MAP()
+};
