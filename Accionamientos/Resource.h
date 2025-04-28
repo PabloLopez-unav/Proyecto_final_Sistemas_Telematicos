e71464d00744845428f4139435dc5d544f35474f
El Consta <you@example.com>
2025-04-28 18:51:22 +0200

    accionamiento hecho menos el boton de start

--- Accionamientos/Resource.h
+++ Accionamientos/Resource.h
@@ -2,18 +2,23 @@
 // Microsoft Visual C++ generated include file.
 // Used by Accionamientos.rc
 //
-#define IDR_MAINFRAME					128
-#define IDD_ACCIONAMIENTOS_DIALOG				102
-#define IDP_SOCKETS_INIT_FAILED			103
+#define IDD_ACCIONAMIENTOS_DIALOG       102
+#define IDP_SOCKETS_INIT_FAILED         103
+#define IDR_MAINFRAME                   128
+#define IDC_START                       1000
+#define IDC_BOT_FRENO                   1001
+#define IDC_PORT                        1001
+#define IDC_BOTON_FRENO                 1005
+#define IDC_BOT_INT_IZQ                 1006
+#define IDC_BOT_INT_DER                 1007
 
 // Next default values for new objects
-//
+// 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-
-#define _APS_NEXT_RESOURCE_VALUE	129
-#define _APS_NEXT_CONTROL_VALUE		1000
-#define _APS_NEXT_SYMED_VALUE		101
-#define _APS_NEXT_COMMAND_VALUE		32771
+#define _APS_NEXT_RESOURCE_VALUE        130
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1006
+#define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
